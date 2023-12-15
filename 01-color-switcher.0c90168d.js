5c7ea032b8d532fefaaad693815955142e34ea50
Shunii06 <138370309+Shunii06@users.noreply.github.com>
2023-12-15 02:51:48 +0000

    Deploying to gh-pages from @ Shunii06/goit-js-hw-09@cc5ede6be2b29a962535221fc8fb267c6bbbaf92 🚀

---
+++ 01-color-switcher.0c90168d.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),d=function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))};e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,t=setInterval(d,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(t)}))}));
+//# sourceMappingURL=01-color-switcher.0c90168d.js.map
